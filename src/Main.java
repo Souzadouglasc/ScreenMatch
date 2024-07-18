59888102df336caf4646f2b6c4730f92628efcda
Souzadouglasc <souza.douglasc@gmail.com>
2024-07-18 11:02:52 -0300

    Inserção do repositorio.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Interestelar");
+
+        int ano;
+        String filme;
+
+        filme = "Interestelar";
+        System.out.println("Meu filme favorito é " + filme);
+    }
+}
